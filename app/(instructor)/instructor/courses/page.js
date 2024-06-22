937deb14bbab38fa11dbdf270d6e5a99e9f0e3c9
Hajavololona Armellin <armelgeek5@gmail.com>
2024-06-22 11:18:36 +0300

    list all course

--- app/(instructor)/instructor/courses/page.js
+++ app/(instructor)/instructor/courses/page.js
@@ -4,16 +4,30 @@ import {auth} from "@clerk/nextjs/server";
 import {redirect} from "next/navigation";
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
-
-export default function CoursePage() {
+import {DataTable} from "@/components/custom/DataTable";
+import { columns } from "@/components/courses/Columns";
+import {db} from "@/lib/db";
+export default async function CoursePage() {
     const { userId } = auth();
 
-    if(!userId) {
-        return redirect('/sign-in');
+    if (!userId) {
+        return redirect("/sign-in");
     }
+
+    const courses = await db.course.findMany({
+        where: {
+            instructorId: userId,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
     return (
         <div className="px-6 py-4">
-           <Link href="/instructor/create-course"><Button>Create new course</Button></Link>
+            <Link href="/instructor/create-course"><Button>Create new course</Button></Link>
+            <div className="mt-5">
+                <DataTable columns={columns} data={courses}/>
+            </div>
         </div>
     );
 }
